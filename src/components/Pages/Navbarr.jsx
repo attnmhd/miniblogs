e010c8b469e0c24b416c2196bb17470c4a8d41fd
natta <muhammadattan900@gmail.com>
2024-06-20 07:15:33 +0700

    dsad

--- src/components/Pages/Navbarr.jsx
+++ src/components/Pages/Navbarr.jsx
@@ -39,7 +39,7 @@ export default function Navbars(){
     <div>    
         <Navbar shadow={false} fullWidth className="border-0">
     <div className="flex items-center justify-between ">
-      <a href="/"><img src="https://miniblogs-phi.vercel.app/src/assets/logo.png" className=" lg:w-72" /></a>
+      <a href="/"><img src="./src/assets/logo.png" className=" lg:w-72" /></a>
       <form action="#" method="GET" class="hidden lg:block relative  right-14">
             <label for="topbar-search" class="sr-only">Search</label>
             <div class="relative lg:w-80 pr-5">
@@ -82,32 +82,26 @@ export default function Navbars(){
         )}
       </IconButton>
     </div>
-    {/* <Collapse open={open}>
+    <Collapse open={open}>
       <div className="container mx-auto mt-3 border-t border-blue-gray-50 px-2 pt-4">
         <ul className="flex flex-col gap-4">
           <NavItem>
-            <RectangleStackIcon className="h-5 w-5" />
-            Pages
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className=" w-5 h-5" >
+<path stroke-linecap="round" stroke-linejoin="round" d="M12 7.5h1.5m-1.5 3h1.5m-7.5 3h7.5m-7.5 3h7.5m3-9h3.375c.621 0 1.125.504 1.125 1.125V18a2.25 2.25 0 0 1-2.25 2.25M16.5 7.5V18a2.25 2.25 0 0 0 2.25 2.25M16.5 7.5V4.875c0-.621-.504-1.125-1.125-1.125H4.125C3.504 3.75 3 4.254 3 4.875V18a2.25 2.25 0 0 0 2.25 2.25h13.5M6 7.5h3v3H6v-3Z" />
+</svg>            Blog
           </NavItem>
           <NavItem>
             <UserCircleIcon className="h-5 w-5" />
             Account
           </NavItem>
-          <NavItem>
-            <Squares2X2Icon className="h-5 w-5" />
-            Blocks
-          </NavItem>
-          <NavItem>
-            <CommandLineIcon className="h-5 w-5" />
-            Docs
-          </NavItem>
+   
         </ul>
         <div className="mt-6 mb-4 flex items-center gap-4">
           <Button variant="text">Log in</Button>
           <Button color="gray">buy now</Button>
         </div>
       </div>
-    </Collapse> */}
+    </Collapse>
   </Navbar></div>
   )
 }
